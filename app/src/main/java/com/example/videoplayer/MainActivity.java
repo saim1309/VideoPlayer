518a9b34c67188bf971b6d01b5c0c1c248181950
Saim Ahmad <Sahmad5349@conestogac.on.ca>
2019-11-21 16:17:12 -0500

    First implementation completed

--- app/src/main/java/com/example/videoplayer/MainActivity.java
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.videoplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    VideoView videoView;
+    MediaController mediaController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView =findViewById(R.id.videoView);
+        mediaController =new MediaController(this);
+        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.vid1);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
     }
 }
